2a08be400b66884b98bb12ad541914f3c2bd3722
minaminii <saraten2@gmail.com>
2023-04-08 16:10:16 +0700

    Create workflow skeleton

--- scripts/workflow/workflow.py
+++ scripts/workflow/workflow.py
@@ -1,10 +1,58 @@
 
-import .components
+import os
+import shutil
+from uuid import uuid4, UUID
+from tempfile import TemporaryDirectory
+from . import components
 
 class Worker(object):
 
     def __init__(self, handler=None) -> None:
         self.handler = handler
 
-    def run():
+    def assign_job(self, input_fastq, output_dir, overwrite=False) -> UUID:
+        '''
+        Assign a job to a worker with provided parameters.
+        '''
+        self.job_id = uuid4()
+        self._input_fastq = input_fastq
+        if os.path.exists(output_dir):
+            if overwrite:
+                os.rmdir(output_dir)
+                os.makedirs(output_dir)
+                return
+        self.output_dir = output_dir
+        return self.job_id
+
+    @classmethod
+    def assign_job_cli(self):
+        '''
+        Command line interface for creating a job.
+        '''
+        fq = input('Path to FASTQ: ')
+        od = input('Output directory: ')
+        if os.path.exists(od):
+            if input('Output directory existed, overwrite? [y/N]: ').lower() != 'y': return
+        return self.assign_job(fq, od, True)
+
+    @classmethod
+    def run_qc(self, input_fastq):
         pass
+
+    def run_workflow(self):
+        with TemporaryDirectory() as _tmpdir:
+            os.symlink(self._input_fastq, f'{_tmpdir.name}/raw_reads.fastq')
+            haplotype = components.strainline(
+                input_fastq=f'{_tmpdir.name}/raw_reads.fastq',
+                output_dir=_tmpdir.name
+            )
+            if haplotype['return_code' == 0]:
+                shutil.move(haplotype['output']['haplotype'], self.ou)
+
+def main():
+    worker = Worker(workplace=os.getcwd())
+    job = worker.assign_job_cli()
+    print(f'Job created (id:{job})')
+
+if __name__ == '__main__':
+    main()
