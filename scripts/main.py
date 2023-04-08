ceb7d48032ab5802d3aaaa00924955e955b412aa
minaminii <saraten2@gmail.com>
2023-04-08 16:09:29 +0700

    Change programname

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,16 @@
 
 import sys
-import utilities.requirements
+from workflow import workflow
 
 PYTHON_VERSION = sys.version_info
 VERSION = "3.10.6"
-PROGRAM = "NanoSim"
+PROGRAM = "HIV64148"
 AUTHOR = "Sara Wattanasombat (Faculty of Medicine, Chiang Mai University, Thailand)"
 CONTACT = "sara_watt@cmu.ac.th"
 
 
 def main():
-    utilities.requirements.check_requirements()
-
+    workflow.main()
+    
 if __name__ == '__main__':
     main()
