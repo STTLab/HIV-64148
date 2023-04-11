6dca8bfdd9abc5f5bccf8549c5a31d0fb7ffb1a4
minaminii <saraten2@gmail.com>
2023-04-11 10:27:21 +0700

    Update simulator.py

--- scripts/tests/simulator.py
+++ scripts/tests/simulator.py
@@ -2,11 +2,12 @@
 import subprocess
 from tempfile import TemporaryDirectory
 
-class simulator(object):
+class Simulator(object):
     def __init__(self) -> None:
-        pass
+        # Create temporary directory
+        self._tmpdir = TemporaryDirectory()
 
-    def simulate_metagenome(self, inputs: list, num_reads: int, model_prefix):
+    def simulate_metagenome(self, inputs: list, num_reads: int, model_prefix, perfect=True):
         '''
         Simulate ONT reads for linear metagenome sample.
         '''
@@ -18,32 +19,49 @@ class simulator(object):
             _abun_sum += item[2]
         if _abun_sum != 100: raise ValueError('Abundance must sum up to 100.')
 
-        # Create temporary directory
-        with TemporaryDirectory() as _temp:
-            # Create metadata
-            genome_list = open(f'{_temp}/ref_list.tsv', 'w')
-            abun_list = open(f'{_temp}/abun_list.tsv', 'w')
-            dna_type_list = open(f'{_temp}/dna_type_list.tsv', 'w')
-            abun_list.write(f'Size\t{num_reads}')
+        # Create metadata
+        genome_list = open(f'{self._tmpdir}/ref_list.tsv', 'w')
+        abun_list = open(f'{self._tmpdir}/abun_list.tsv', 'w')
+        dna_type_list = open(f'{self._tmpdir}/dna_type_list.tsv', 'w')
+        abun_list.write(f'Size\t{num_reads}')
 
-            for name, fasta_path, abun in inputs:
-                genome_list.write(f'{name}\t{fasta_path}\n')
-                abun_list.write(f'{name}\t{abun}\n')
-                dna_type_list.write(f'{name}\tlinear\n')
-            genome_list.close()
-            abun_list.close()
+        for name, fasta_path, abun in inputs:
+            genome_list.write(f'{name}\t{fasta_path}\n')
+            abun_list.write(f'{name}\t{abun}\n')
+            dna_type_list.write(f'{name}\tlinear\n')
+        genome_list.close()
+        abun_list.close()
 
-            genome_list = f'{_temp}/ref_list.tsv'
-            abun_list = f'{_temp}/abun_list.tsv'
-            dna_type_list = f'{_temp}/dna_type_list.tsv'
+        genome_list = f'{self._tmpdir}/ref_list.tsv'
+        abun_list = f'{self._tmpdir}/abun_list.tsv'
+        dna_type_list = f'{self._tmpdir}/dna_type_list.tsv'
 
-            cmd = [
-                'simulator.py', 'metagenome',
-                '--genome_list', genome_list,
-                '--abun', abun_list,
-                '--dna_type_list', dna_type_list,
-                '--model_prefix', model_prefix,
-                '--output', f'{_temp}/simulated'
-            ]
-            subprocess.Popen(cmd)
-    
+        cmd = [
+            'simulator.py', 'metagenome',
+            '--genome_list', genome_list,
+            '--abun', abun_list,
+            '--dna_type_list', dna_type_list,
+            '--model_prefix', model_prefix,
+            '--output', f'{self._tmpdir}/simulated',
+            '--max_len', '12000',
+            '--min_len', '3500',
+            '--basecaller', 'guppy'
+            '--fastq'
+        ]
+        if perfect:
+            cmd.append('--perfect')
+        subprocess.Popen(cmd)
+
+    def _check(self):
+        process = subprocess.Popen(['read_analysis.py', '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if stdout:
+            print(stdout.decode())
+        process = subprocess.Popen(['simulator.py', '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if stdout:
+            print(stdout.decode())
+
+if __name__ == '__main__':
+    sim = Simulator()
+    sim._check()
