fd707c9d528343ec7650778acb5794f69bd2dc47
minaminii <saraten2@gmail.com>
2023-04-12 16:38:55 +0700

    Add secret for NCBI API key

--- scripts/utilities/apis.py
+++ scripts/utilities/apis.py
@@ -6,31 +6,54 @@ from Bio import SeqIO
 from io import StringIO
 from workflow.gen_report import read_haplotype_fa
 from utilities.logger import logger
+from utilities.settings import secrets
 
-class EutilsNCBI():
+class EutilsNCBI:
+
+    API_KEY = secrets.get('API_KEYS', {}).get('NCBI', '____YOUR_KEY____')
+    END_POINT = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
     def __init__(self) -> None:
         pass
 
     @classmethod
     def fetch_fasta(cls, accession, save_to=None):
-        url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
         params = {
             'db': 'nucleotide',
             'id': accession,
             'retmode': 'fasta',
             'rettype': 'fasta'
         }
+        if cls.API_KEY != '____YOUR_KEY____': params['api_key'] = cls.API_KEY
         logger.debug(f'Fetching sequence {accession}')
-        res = requests.get(f'{url}/efetch.fcgi', params=params)
+        res = requests.get(f'{cls.END_POINT}/efetch.fcgi', params=params)
         try:
             seq = SeqIO.read(StringIO(res.content.decode()), 'fasta')
+            summary: dict = cls.fetch_summery(accession)
+            seq.description = f'{summary.get("subname","_|_").split("|")[0]}; {summary.get("title", "")}'
         except Exception as e:
             raise e
         if save_to:
             SeqIO.write(seq, save_to, 'fasta')
             return
         return seq
-
+    
+    @classmethod
+    def fetch_summery(cls, accession):
+        params = {
+            'db': 'nucleotide',
+            'id': accession,
+            'retmode': 'json'
+        }
+        if cls.API_KEY != '____YOUR_KEY____': params['api_key'] = cls.API_KEY
+        else: logger.info('NCBI key not setup. Please input your key to increase API limit.')
+        logger.debug(f'Fetching summary for accession number: {accession}')
+        res = requests.get(f'{cls.END_POINT}/esummary.fcgi', params=params)
+        try:
+            data = res.json()
+            data = data['result'][data['result']['uids'][0]]
+        except Exception as e:
+            raise e
+        return data
 
 def hivdb_seq_analysis(haplotypes):
     sequences = []
--- scripts/utilities/settings.py
+++ scripts/utilities/settings.py
@@ -11,5 +11,14 @@ with open(SETTINGS, 'r') as f:
     global settings
     settings = dict(json.load(f))
 
+allow_secrets = [f'{os.getcwd()}\\settings\\secrets.json', f'/hiv64148/settings/secrets.json', f'{os.getcwd()}/settings/secrets.json']
+for p in allow_secrets:
+    if os.path.exists(p):
+        SECRETS = p
+
+with open(SECRETS, 'r') as f:
+    global secrets
+    secrets = dict(json.load(f))
+
 if __name__ == '__main__':
     print(settings)
