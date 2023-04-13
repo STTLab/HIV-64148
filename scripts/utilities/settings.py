db6912c2a2a8619e663587456d6484ed4cee5ecd
minaminii <saraten2@gmail.com>
2023-04-13 20:10:30 +0700

    Add ability to raise exception

--- scripts/utilities/settings.py
+++ scripts/utilities/settings.py
@@ -2,23 +2,24 @@
 import os
 import json
 
+SETTINGS, SECRETS = None, None
 allow_settings = [f'{os.getcwd()}\\settings\\settings.json', f'/hiv64148/settings/settings.json', f'{os.getcwd()}/settings/settings.json']
 for p in allow_settings:
     if os.path.exists(p):
         SETTINGS = p
-
-with open(SETTINGS, 'r') as f:
-    global settings
-    settings = dict(json.load(f))
+        with open(SETTINGS, 'r') as f:
+            global settings
+            settings = dict(json.load(f))
+        break
 
 allow_secrets = [f'{os.getcwd()}\\settings\\secrets.json', f'/hiv64148/settings/secrets.json', f'{os.getcwd()}/settings/secrets.json']
 for p in allow_secrets:
     if os.path.exists(p):
         SECRETS = p
+        with open(SECRETS, 'r') as f:
+            global secrets
+            secrets = dict(json.load(f))
+        break
 
-with open(SECRETS, 'r') as f:
-    global secrets
-    secrets = dict(json.load(f))
-
-if __name__ == '__main__':
-    print(settings)
+if SETTINGS is None: raise FileExistsError('Cannot fine settings.json')
+if SECRETS is None: raise FileExistsError('Cannot find secret.json')
