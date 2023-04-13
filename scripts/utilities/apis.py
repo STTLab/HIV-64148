6a662f5d3f05a9730f937b764b572e7ce013cf6f
minaminii <saraten2@gmail.com>
2023-04-14 01:42:36 +0700

    Multiprocessing on ncbi request

--- scripts/utilities/apis.py
+++ scripts/utilities/apis.py
@@ -1,9 +1,10 @@
 
-from sierrapy import SierraClient
-from sierrapy.sierraclient import Sequence
 import requests
 from Bio import SeqIO
 from io import StringIO
+import multiprocessing
+from sierrapy import SierraClient
+from sierrapy.sierraclient import Sequence
 from workflow.gen_report import read_haplotype_fa
 from utilities.logger import logger
 from utilities.settings import secrets
@@ -29,7 +30,16 @@ class EutilsNCBI:
         try:
             seq = SeqIO.read(StringIO(res.content.decode()), 'fasta')
             summary: dict = cls.fetch_summery(accession)
-            seq.description = f'{summary.get("subname","_|_").split("|")[0]}; {summary.get("title", "")}'
+            info = dict(zip(summary.get("subtype","").split('|'), summary.get("subname","").split("|")))
+            if 'subtype' in info.keys():
+                subtype = info['subtype']
+            else: subtype = 'subtype ' + hivdb_seq_analysis(
+                                Sequence(header=seq.id, sequence=str(seq.seq)),
+                                'inputSequence { header, SHA512 }, strain { name }, \
+                                bestMatchingSubtype { displayWithoutDistance, distance }'
+                            )[0]['bestMatchingSubtype']['displayWithoutDistance']
+            
+            seq.description = f'{subtype}; {summary.get("title", "")}'
         except Exception as e:
             raise e
         if save_to:
@@ -54,19 +64,40 @@ class EutilsNCBI:
         except Exception as e:
             raise e
         return data
-
-def hivdb_seq_analysis(haplotypes):
-    sequences = []
-    query = open('./scripts/utilities/gql/hiv1_sequence_analysis_default.gql', 'r').read()
-    for record in haplotypes:
-        sequences.append(Sequence(header=record.id, sequence=str(record.seq)))
     
+    @classmethod
+    def fetch_fasta_parallel(cls, *accession, save_to=None):
+        if cls.API_KEY != '____YOUR_KEY____': nproc = 10
+        else:
+            nproc = 3
+            logger.info('NCBI key not setup. Please input your key to increase API limit.')
+        with multiprocessing.Pool(processes=nproc) as pool:
+            seqs = (pool.map(cls.fetch_fasta, accession[0]))
+        if save_to:
+            SeqIO.write(seqs, save_to, 'fasta')
+            return
+        if len(seqs) == 1: return seqs[0]
+        return seqs
+
+def hivdb_seq_analysis(sequence, gql_query):
     client: SierraClient = SierraClient()
-    return client.sequence_analysis(sequences, query)
+    return client.sequence_analysis([sequence], gql_query)
         
 
 def _test():
-    print(EutilsNCBI.fetch_fasta('KC492737'))
+    accession_list = [
+            'AF164485.1', 'AF259954.1', 'AF259955.1', 'KU168309.1', 'U51189.1',
+            'AB049811.1', 'AB231895.1', 'KC492737', 'KC492738', 'KC503852',
+            'KC503853', 'KC503854', 'KC503855', 'KF234628', 'AF385934',
+            'AF385935', 'AF385936', 'AB703607.1', 'EF158040.1', 'AF324493.2',
+            'MH705157.1', 'AY835759.1', 'K03455.1', 'X01762.1', 'AY772699.1',
+            'AB485648.1', 'AJ320484.1', 'KY392769.1', 'AB231893.1', 'AB485662.1',
+            'AF084936.1', 'MH705134.1'
+        ]
+    # print(EutilsNCBI.fetch_fasta_parallel(accession_list[:5]))
+    # print(EutilsNCBI.fetch_fasta_parallel('AF164485.1'))
+    # seqs = [EutilsNCBI.fetch_fasta(accession) for accession in accession_list]
+    # SeqIO.write(seqs, f'32_HIV1_for_BLAST.fasta', 'fasta')
     # print(hivdb_seq_analysis(read_haplotype_fa('scripts\\tests\\mock\\HIV1_Strainline_result.fa')))
 
 if __name__ == '__main__':
