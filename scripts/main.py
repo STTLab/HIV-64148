1f3cb29eabaae1e5dd0181f270982962ded61696
minaminii <saraten2@gmail.com>
2023-04-15 12:31:37 +0700

    Add args

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,9 @@
 
 import sys
-from utilities import apis
-from workflow import workflow
+import argparse
+from workflow.workflow import Worker
+from utilities.logger import logger 
+from utilities.requirements import setup_workflow
 
 PYTHON_VERSION = sys.version_info
 VERSION = "3.10.6"
@@ -11,8 +13,22 @@ CONTACT = "sara_watt@cmu.ac.th"
 
 
 def main():
-    # workflow.main()
-    apis._test()
+    parser = argparse.ArgumentParser(
+                prog='HIV-64148 Pipeline',
+                description='What the program does',
+                epilog='Text at the bottom of help')
+    parser.add_argument('function')
+
+    args = parser.parse_args()
+    match args.function:
+        case 'run_cli':
+            worker = Worker()
+            job = worker.assign_job_cli()
+            logger.info(f'Job created (id:{job})')
+            worker.run_workflow()
+        case 'setup':
+            setup_workflow()
+        case _: parser.print_help()
     
 if __name__ == '__main__':
     main()
