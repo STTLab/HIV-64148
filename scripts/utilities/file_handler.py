888380fb4ee806a68a1eaf240283a700adc5ff89
minaminii <saraten2@gmail.com>
2023-04-16 10:03:39 +0700

    Reorganize file gandling

--- scripts/utilities/file_handler.py
+++ scripts/utilities/file_handler.py
@@ -5,22 +5,28 @@ from Bio import SeqIO
 # from .logger import logger
 
 class FASTA(object):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, sequences) -> None:
+        self.sequences = sequences
 
     @classmethod
-    def read(cls, file) -> dict[str, SeqIO.SeqRecord]:
+    def read(cls, file):
         if isinstance(file, FileIO): filename = file.name
         else: filename = file
         # logger.debug(f'Reading sequences from {filename}')
         cls.sequences: dict[str, SeqIO.SeqRecord] = SeqIO.to_dict(SeqIO.parse(file, 'fasta'))
-        return cls.sequences
+        return FASTA(cls.sequences)
     
     @classmethod
-    def read_and_extract(cls, file, id, save_to: str|None = None) -> SeqIO.SeqRecord:
-        seq = cls.read(file)[id]
-        if save_to: SeqIO.write(seq, save_to, 'fasta')
+    def read_and_extract(cls, file, id, save_to: str|None = None) -> str | SeqIO.SeqRecord:
+        seq = cls.read(file).extract(id, save_to)
         return seq
 
-    def extract(self, id: str) -> SeqIO.SeqRecord:
-        return self.sequences[id]
+    def extract(self, id: str, save_to: str|None = None) -> SeqIO.SeqRecord | str:
+        seq = self.sequences[id]
+        if save_to:
+            SeqIO.write(seq, save_to, 'fasta')
+            return save_to
+        return seq
+    
+    def list_ids(self):
+        return tuple(self.sequences.keys())
