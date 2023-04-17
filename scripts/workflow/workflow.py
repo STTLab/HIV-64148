75a8cb43878b2853fbec5d7d518b6f342bcf9a98
minaminii <saraten2@gmail.com>
2023-04-17 17:03:51 +0700

    Add mem recorder and generate html report

--- scripts/workflow/workflow.py
+++ scripts/workflow/workflow.py
@@ -1,19 +1,33 @@
 
-import os, re
+import os
 import glob
+import time
 import shutil
+import tracemalloc
 from Bio import SeqIO
+from pathlib import Path
 from uuid import uuid4, UUID
 from tempfile import TemporaryDirectory
 from utilities.logger import logger
 from utilities.settings import settings
 from utilities.file_handler import FASTA
-from .components import BLAST, strainline, minimap2, snippy
+from .components import BLAST, strainline, snippy, nanoplot_qc
+from .gen_report import generate_report_skeleton
 
 class Worker(object):
 
     def __init__(self, handler=None) -> None:
         self.handler = handler
+        self._stat = {
+            't_created': time.time(),
+            'peak_mem': {}
+        }
+
+    def get_peak_mem(self) -> dict:
+        return self._stat.get('peak_mem', {})
+    
+    def get_runtime(self):
+        return time.time() - self._stat.get('t_start', time.time())
 
     def assign_job(self, input_fastq, output_dir, overwrite=False) -> UUID | None:
         '''
@@ -41,11 +55,24 @@ class Worker(object):
         return self.assign_job(fq, od, True)
 
     @classmethod
-    def run_qc(cls, input_fastq):
-        pass
+    def run_qc(cls, input_file, output_dir):
+        match Path(input_file).suffix:
+            case '.fa' | '.fasta':
+                input_type = 'fasta'
+            case '.fastq':
+                input_type = 'fastq'
+            case _:
+                return
+        return nanoplot_qc(input_file, input_type, output_dir)
 
     def run_workflow(self):
+        tracemalloc.start()
+        self._stat['t_start'] = time.time()
+
         if not os.path.exists(self.output_dir): os.makedirs(self.output_dir)
+        self.run_qc(self._input_fastq, f'{self.output_dir}/qc')
+
+        # Strainline
         with TemporaryDirectory() as _tmpdir:
             os.symlink(self._input_fastq, f'{_tmpdir}/raw_reads.fastq')
             strainline(
@@ -56,9 +83,19 @@ class Worker(object):
             [shutil.move(file, self.output_dir) for file in glob.glob(f'{_tmpdir}/*')]
             logger.info('Finished - Strainline')
 
+        # Log memory
+        _, _peak = tracemalloc.get_traced_memory()
+        self._stat['peak_mem']['strainline'] = _peak
+        tracemalloc.reset_peak()
+
         # BLAST
         blast = BLAST.blast_nucleotide(f'{self.output_dir}/haplotypes.final.fa', '32hiv1_default_db', self.output_dir)
         logger.info('Finished - BLASTN')
+
+        # Log memory
+        _, _peak = tracemalloc.get_traced_memory()
+        self._stat['peak_mem']['blast'] = _peak
+        tracemalloc.reset_peak()
 
         logger.info('Perform variant calling...')
         # Extract sequence according to BLAST result
@@ -66,14 +103,12 @@ class Worker(object):
         haplotype_seqs = FASTA.read(f'{self.output_dir}/haplotypes.final.fa')
         rep_seqs = FASTA.read(settings['data']['variant_calling']['rep_fasta'])
         rep_ids = settings['data']['variant_calling']['rep_ids']
-        subtype_regex = re.compile(r'CRF[0-9]{2}_[A-Z]{2}|subtype_[A,B,C,D,F1,F2,G]')
         os.mkdir(f'{self.output_dir}/variants')
         for hap, iden in blast_result:
             with TemporaryDirectory() as _temp:
                 seq = haplotype_seqs.extract(hap)
                 SeqIO.write(seq, f'{_temp}/{hap}.fasta', 'fasta')
-                iden_seq = FASTA.read_and_extract(f"{settings['data']['blast']['db_dir']}/32hiv1_default_db", iden)
-                subtype = subtype_regex.findall(iden_seq.description)[0]
+                subtype = BLAST.get_subtypes('32hiv1_default_db', iden)
                 logger.info(f'Haplotype {hap} identified as {iden}, {subtype}')
                 rep_seqs.extract(rep_ids[subtype], save_to=f'{_temp}/{subtype}.fasta')
                 snippy(
@@ -84,3 +119,11 @@ class Worker(object):
                     tmp_dir=_temp
                 )
         logger.info('Finished - Variant calling')
+
+        # Log memory
+        _, _peak = tracemalloc.get_traced_memory()
+        self._stat['peak_mem']['snippy'] = _peak
+        tracemalloc.stop()
+        
+        logger.info('Generating report')
+        generate_report_skeleton(self.job_id, f'{self.output_dir}/haplotypes.final.fa', f'{self.output_dir}/hiv-64148_report.html', f'{self.output_dir}/qc/{Path(self._input_fastq).stem}_NanoPlot-report.html', self)
