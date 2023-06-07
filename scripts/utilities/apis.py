7b3efd81b33f32a7a23cb407e9c655d308a42517
Sara Wattanasombat <saraten2@gmail.com>
2023-06-07 16:25:13 +0700

    Reformat code

--- scripts/utilities/apis.py
+++ scripts/utilities/apis.py
@@ -1,14 +1,13 @@
 
-import requests
-import pandas as pd
-from Bio import SeqIO
 import multiprocessing
 from io import StringIO
 from uuid import uuid4
-from yattag.doc import Doc
-from yattag.indentation import indent
+from Bio import SeqIO
+import pandas as pd
+import requests
 from sierrapy import SierraClient
 from sierrapy.sierraclient import Sequence
+import json
 from functools import lru_cache
 from utilities.logger import logger
 from utilities.settings import secrets
@@ -34,13 +33,13 @@ class EutilsNCBI:
             'rettype': 'fasta'
         }
         if cls.API_KEY != '____YOUR_KEY____': params['api_key'] = cls.API_KEY
-        logger.debug(f'Fetching sequence {accession}')
-        res = requests.get(f'{cls.END_POINT}/efetch.fcgi', params=params)
+        logger.debug('Fetching sequence %s', accession)
+        res = requests.get(f'{cls.END_POINT}/efetch.fcgi', params=params, timeout=10)
         seq = SeqIO.read(StringIO(res.content.decode()), 'fasta')
         if subtype is None:
             try:
                 summary: dict = cls.fetch_summery(accession)
-                info = dict(zip(summary.get("subtype","").split('|'), summary.get("subname","").split("|")))
+                info = dict(zip(summary.get('subtype','').split('|'), summary.get('subname','').split('|')))
                 if 'subtype' in info.keys():
                     subtype = 'subtype_' + info['subtype']
                 else:
@@ -66,9 +65,10 @@ class EutilsNCBI:
             'id': accession,
             'retmode': 'json'
         }
-        if cls.API_KEY != '____YOUR_KEY____': params['api_key'] = cls.API_KEY
+        if cls.API_KEY != '____YOUR_KEY____':
+            params['api_key'] = cls.API_KEY
         else: logger.info('NCBI key not setup. Please input your key to increase API limit.')
-        logger.debug(f'Fetching summary for accession number: {accession}')
+        logger.debug('Fetching summary for accession number: %s', accession)
         res = requests.get(f'{cls.END_POINT}/esummary.fcgi', params=params)
         try:
             data = res.json()
@@ -79,26 +79,30 @@ class EutilsNCBI:
 
     @classmethod
     def fetch_fasta_parallel(cls, *accession, save_to=None):
-        if cls.API_KEY != '____YOUR_KEY____': nproc = 10
+        if cls.API_KEY != '____YOUR_KEY____':
+            nproc = 10
         else:
             nproc = 3
             logger.info('NCBI key not setup. Please input your key to increase API limit.')
         with multiprocessing.Pool(processes=nproc) as pool:
-            seqs = (pool.map(cls.fetch_fasta, accession[0]))
+            seqs = pool.map(cls.fetch_fasta, accession[0])
         if save_to:
             SeqIO.write(seqs, save_to, 'fasta')
             return
         if len(seqs) == 1: return seqs[0]
         return seqs
 
-def hivdb_seq_analysis(sequences, gql_query):
+def hivdb_seq_analysis(sequences, gql_query, save_to=None):
     client: SierraClient = SierraClient()
     try :
         response = client.sequence_analysis([*sequences], gql_query)
+        if save_to:
+            with open(save_to, 'w', encoding='utf-8') as file:
+                json.dump(response, file)
         conv_to_result = [ SequenceAnalysisResult(data=result) for result in response ]
         return conv_to_result
     except ConnectionError:
-        return
+        return None
 
 class SequenceAnalysisResult(object):
     def __init__(self, data:dict, **kwargs) -> None:
@@ -107,9 +111,15 @@ class SequenceAnalysisResult(object):
         self.bestMatchingSubtype = data['bestMatchingSubtype']
         self.drugResistance = self.DrugResistantProfile(data['drugResistance'])
         self.mutations = self.MutationProfile(data['mutations'])
-        self.alignGeneSequences = [ self.AlignSequences(aln['gene']['name'], aln['matchPcnt'], aln['prettyPairwise'], aln['mutations']) for aln in data['alignedGeneSequences']]
+        self.alignGeneSequences = [
+            self.AlignSequences(aln['gene']['name'],
+                                aln['matchPcnt'],
+                                aln['prettyPairwise'],
+                                aln['mutations']
+                                ) for aln in data['alignedGeneSequences']
+        ]
         self.validationResults = data['validationResults']
-        return
+
 
     class DrugResistantProfile(object):
         def __init__(self, data: dict) -> None:
@@ -165,7 +175,7 @@ class SequenceAnalysisResult(object):
                 message = 'Unusual mutation'
             else:
                 message = 'Mutation'
-            message += ' at AA position {}, from {} to {}'.format(pos, ref, alt)
+            message += f' at AA position {pos}, from {ref} to {alt}'
             if is_Apobec:
                 message += ' caused by APOBEC'
             message += '.'
@@ -220,8 +230,10 @@ class SequenceAnalysisResult(object):
                     pretty += '\n'
                     f=t
                     t+=increment
-                    if t > len(items): t = len(items)
-                    if f>=len(items): break
+                    if t > len(items):
+                        t = len(items)
+                    if f>=len(items):
+                        break
                 return pretty
 
             def to_string(self):
@@ -250,7 +262,7 @@ class SequenceAnalysisResult(object):
                         html += '\t\t\t<td>\n'
                         html += f'\t\t\t\t<br>{str(f+1)}'
                     html += '\t\t\t</td>'
-                    for pos, ref, align, mutation in items[f:t]:
+                    for _, ref, align, mutation in items[f:t]:
                         if mutation.strip() == '-':
                             html += '\t\t\t<td>\n'
                         else:
@@ -280,22 +292,3 @@ class SequenceAnalysisResult(object):
                 html += '\t</tbody>'
                 html += '</table>'
                 return html
-
-def _test():
-    accession_list = [
-            'AF164485.1', 'AF259954.1', 'AF259955.1', 'KU168309.1', 'U51189.1',
-            'AB049811.1', 'AB231895.1', 'KC492737', 'KC492738', 'KC503852',
-            'KC503853', 'KC503854', 'KC503855', 'KF234628', 'AF385934',
-            'AF385935', 'AF385936', 'AB703607.1', 'EF158040.1', 'AF324493.2',
-            'MH705157.1', 'AY835759.1', 'K03455.1', 'X01762.1', 'AY772699.1',
-            'AB485648.1', 'AJ320484.1', 'KY392769.1', 'AB231893.1', 'AB485662.1',
-            'AF084936.1', 'MH705134.1'
-        ]
-    # print(EutilsNCBI.fetch_fasta_parallel(accession_list[:5]))
-    # print(EutilsNCBI.fetch_fasta_parallel('AF164485.1'))
-    # seqs = [EutilsNCBI.fetch_fasta(accession) for accession in accession_list]
-    # SeqIO.write(seqs, f'32_HIV1_for_BLAST.fasta', 'fasta')
-    # print(hivdb_seq_analysis(SeqIO.parse('scripts\\tests\\mock\\HIV1_Strainline_result.fa')))
-
-if __name__ == '__main__':
-    _test()
